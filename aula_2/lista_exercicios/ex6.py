befbea37148c1e61fcd4cff1b1333865b8bdb436
Gabriela Rodrigues <gabriela.souza.investimentos@gmail.com>
2026-08-18 21:49:51 +0000

    feat: Continuando exercícios

--- aula_2/lista_exercicios/ex6.py
+++ aula_2/lista_exercicios/ex6.py
@@ -1,3 +1,14 @@
-limite = int(input("Digite um número(maior ou igual a 1): "))
-divisor = int(input("Digite um número(diferente de 0): "))
+## Problema 6 - Soma de múltiplos sem divisor zero
 
+limite = int(input("Digite um limite(maior ou igual a 1): "))
+divisor = int(input("Digite um divisor(diferente de 0): "))
+
+acumulador = 0
+
+for numero in range(1,limite+1):
+    if numero % divisor == 0:
+        acumulador += numero
+
+        print(f'{numero}')
+
+print(f'Soma: {acumulador}')
